db7ab4671f9a36ec143b75454706870110de1c20
MCVNick <mcvnick@gmail.com>
2019-02-09 08:09:26 -0700

    +file structure +initial server setup

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
+const express = require('express')
+const { json } = require('express')
+const massive = require('massive')
+const session = require('express-session')
+
+const authCtrl = require('./controller/auth')
+
+const app = express()
+
+app.use(json())
+app.use(express.static('build'))
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+massive(CONNECTION_STRING)
+    .then(db => {
+        app.set('db', db)
+        const port = SERVER_PORT || 3005
+        app.listen(port, console.log('The server is running on port', port))
+    })
+
+//Authentication endpoints
