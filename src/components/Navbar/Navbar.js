a1972f2e53e204b14366f6e7d89e8b7a304bf8a0
MCVNick <mcvnick@gmail.com>
2019-02-21 09:52:04 -0700

    finished the weather dropdown

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,8 @@ class Navbar extends Component {
             provoObj: {},
             ogdenObj: {},
             stGeorgeObj: {},
-            loganObj: {}
+            loganObj: {},
+            pencilClicks: 0
         }
     }
 
@@ -34,12 +35,61 @@ class Navbar extends Component {
                     stGeorgeObj,
                     loganObj
                 })
+                console.log({ saltLakeObj, provoObj, ogdenObj, stGeorgeObj, loganObj })
             })
             .catch(error => {})
     }
 
     getWeatherIcon(icon) {
         return `http://openweathermap.org/img/w/${icon}.png`
+    }
+
+    handleChangeCity(name1 = 'saltLakeObj', name2 = 'Salt Lake') {
+        this.setState({
+            selectedTemp: name1,
+            selectedTempName: name2
+        })
+
+        this.handleExpandCity()
+    }
+
+    handleTime(num) {
+        if (num - 8 < 0) {
+            num = num + 24
+        }
+        num -= 8
+
+        if (num - 12 < 0) {
+            return `${num}AM`
+        } else {
+            return `${num - 12}PM`
+        }
+    }
+
+    handleExpandCity() {
+        const { pencilClicks } = this.state
+        let expand = document.getElementById('expand-select-city')
+        let weather = document.getElementById('weather-overlay')
+
+        this.setState({
+            pencilClicks: pencilClicks + 1
+        })
+
+        if(expand.classList.contains('expand-select-city')) {
+            expand.classList.remove('expand-select-city')
+        } else {
+            expand.classList.add('expand-select-city')
+        }
+    }
+
+    openWeather() {
+        let weather = document.getElementById('weather-overlay')
+
+        if(weather.classList.contains('weather-overlay-slid')) {
+            weather.classList.remove('weather-overlay-slid')
+        } else {
+            weather.classList.add('weather-overlay-slid')
+        }
     }
 
     render() {
@@ -66,36 +116,42 @@ class Navbar extends Component {
                     </Link>
                     <div className='flex-row center-all'>
                         <div className='flex-row auto-left'>
-                            <div className='flex-row center-all weather-icon-parent'><img className='nav-weather-icon' src={this.getWeatherIcon(this.state[selectedTemp].list ? this.state[selectedTemp].list[0].weather[0].icon : null)} alt='weather'></img></div>
-                            <div className='flex-row center-all weather-temp'>{this.state[selectedTemp].list ? Math.round(this.state[selectedTemp].list[0].main.temp) : null}°</div>
-                            <div id='weather-overlay' className='flex-row auto-left'>
-                                <div className='selected-location'>
-                                    <h1>{this.state.selectedTempName}</h1>
-                                    <i className="fas fa-pencil-alt auto-left"></i>
+                            <div className='flex-row center-all weather-icon-parent' onClick={() => this.openWeather()}><img className='nav-weather-icon' src={this.getWeatherIcon(this.state[selectedTemp].list ? this.state[selectedTemp].list[0].weather[0].icon : null)} alt='weather'></img></div>
+                            <div className='flex-row center-all weather-temp' onClick={() => this.openWeather()}>{this.state[selectedTemp].list ? Math.round(this.state[selectedTemp].list[0].main.temp) : null}°</div>
+                            <div id='weather-overlay' className='flex-row auto-left weather-overlay-slid'>
+                                <div className='prevent-click-on-other-content' onClick={() => this.openWeather()}></div>
+                                <div className='weather-overlay-content'>
+                                    <div className='selected-location' onClick={() => this.handleExpandCity()}>
+                                        <h1>{this.state.selectedTempName}</h1>
+                                        <i className="fas fa-pencil-alt auto-left"></i>
+                                    </div>
+                                    <ul id='expand-select-city'>
+                                        <li onClick={() => {this.handleChangeCity()}}>Salt Lake</li>
+                                        <li onClick={() => {this.handleChangeCity('provoObj', 'Provo')}}>Provo</li>
+                                        <li onClick={() => {this.handleChangeCity('ogdenObj', 'Ogden')}}>Ogden</li>
+                                        <li onClick={() => {this.handleChangeCity('stGeorgeObj', 'St. George')}}>St. George</li>
+                                        <li onClick={() => {this.handleChangeCity('loganObj', 'Logan')}}>Logan</li>
+                                    </ul>
+                                    <div className='weather-forecast-expanded'>
+                                        <div className='forecast-large-weather'>
+                                            <img src={this.getWeatherIcon(this.state[selectedTemp].list ? this.state[selectedTemp].list[0].weather[0].icon : null)} alt='weather'></img>
+                                        </div>
+                                        <h1 className='forecast-large-temp'>{this.state[selectedTemp].list ? Math.round(this.state[selectedTemp].list[0].main.temp) : null}°</h1>
+                                        <h2 className='forecast-time-a'>{this.state[selectedTemp].list ? this.handleTime(parseInt(this.state[selectedTemp].list[0].dt_txt.slice(11, 13))) : null}</h2>
+                                        <img className='forecast-img-a' src={this.getWeatherIcon(this.state[selectedTemp].list ? this.state[selectedTemp].list[0].weather[0].icon : null)} alt='weather'></img>
+                                        <h2 className='forecast-temp-a'>{this.state[selectedTemp].list ? Math.round(this.state[selectedTemp].list[0].main.temp) : null}°</h2>
+                                        <h2 className='forecast-time-b'>{this.state[selectedTemp].list ? this.handleTime(parseInt(this.state[selectedTemp].list[1].dt_txt.slice(11, 13))) : null}</h2>
+                                        <img className='forecast-img-b' src={this.getWeatherIcon(this.state[selectedTemp].list ? this.state[selectedTemp].list[1].weather[0].icon : null)} alt='weather'></img>
+                                        <h2 className='forecast-temp-b'>{this.state[selectedTemp].list ? Math.round(this.state[selectedTemp].list[2].main.temp) : null}°</h2>
+                                        <h2 className='forecast-time-c'>{this.state[selectedTemp].list ? this.handleTime(parseInt(this.state[selectedTemp].list[2].dt_txt.slice(11, 13))) : null}</h2>
+                                        <img className='forecast-img-c' src={this.getWeatherIcon(this.state[selectedTemp].list ? this.state[selectedTemp].list[2].weather[0].icon : null)} alt='weather'></img>
+                                        <h2 className='forecast-temp-c'>°{this.state[selectedTemp].list ? Math.round(this.state[selectedTemp].list[3].main.temp) : null}</h2>
+                                        <h2 className='forecast-humidity'>Humidity {this.state[selectedTemp].list ? this.state[selectedTemp].list[0].main.humidity : null}%</h2>
+                                        <h2 className='forecast-info'>{this.state[selectedTemp].list ? this.state[selectedTemp].list[0].weather[0].description : null}</h2>
+                                        <a className='forecast-button' href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'><button>Full Forecast</button></a>
+                                    </div>
                                 </div>
-                                <ul>
-                                    <li>Salt Lake</li>
-                                    <li>Provo</li>
-                                    <li>Ogden</li>
-                                    <li>St. George</li>
-                                    <li>Logan</li>
-                                </ul>
-                                <div className='weather-forecast-expanded'>
-                                    <img className='forecast-large-weather' src={this.getWeatherIcon(this.state[selectedTemp].list ? this.state[selectedTemp].list[0].weather[0].icon : null)} alt='weather'></img>
-                                    <h1 className='forecast-large-temp'>{this.state[selectedTemp].list ? Math.round(this.state[selectedTemp].list[0].main.temp) : null}°</h1>
-                                    <h2 className='forecast-time-a'></h2>
-                                    <img className='forecast-img-a'></img>
-                                    <h2 className='forecast-temp-a'>°</h2>
-                                    <h2 className='forecast-time-b'></h2>
-                                    <img className='forecast-img-b'></img>
-                                    <h2 className='forecast-temp-b'>°</h2>
-                                    <h2 className='forecast-time-c'></h2>
-                                    <img className='forecast-img-c'></img>
-                                    <h2 className='forecast-temp-c'>°</h2>
-                                    <h2 className='forecast-humidity'></h2>
-                                    <h2 className='forecast-info'></h2>
-                                    <button className='forecast-button'>Full Forecast</button>
-                                </div>
+
                             </div>
                         </div>
                         {
